911c319923535a0d63d29d7697dfeb2abd8b29b5
yashar6846 <yasharnajafi6846@gmail.com>
2021-05-12 08:39:55 +0300

    Add to shopping Data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "./Component/Cart";
 import Manu from "./Component/Menu/Menu";
 import Products from "./Component/Products";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((cart) => [...cart, product]);
+  };
+
   return (
     <div>
-      <Manu />
-      <Products />
-      <Cart />
+      <Manu cartCount={cart.langth} />
+      <Products addToCart={addToCart} />
+      <Cart cart={cart} />
     </div>
   );
 }
