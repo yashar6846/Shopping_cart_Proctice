bd7cd37aa64bfb62ed1fb9dfd3fd7ef4f2180c75
yashar6846 <yasharnajafi6846@gmail.com>
2021-05-12 17:26:38 +0300

    commit add cart

---
+++ src/Component/Cart/styled.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flexbox;
+  flex-wrap: wrap;
+`;
+
+export const Image = styled.img`
+  width: 50px;
+  height: 50px;
+`;
+export const TextBody = styled.div`
+  margin: 0 15px;
+`;
+export const Title = styled.h3`
+  font-size: 24px;
+`;
+export const Description = styled.h5`
+  font-size: 15px;
+`;
+export const Price = styled.div`
+  font-size: 15px;
+`;
+export const Button = styled.button`
+  border-radius: 5px;
+  background-color: #2245dd;
+  color: white;
+  padding: 5px 10px;
+  &:hover {
+    background-color: #554599;
+  }
+`;
+export const ProductContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  border: 1px solid gray;
+  border-radius: 5px;
+  width: 350px;
+  margin: 15px;
+`;
--- src/Component/Menu/Menu.js
+++ src/Component/Menu/Menu.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Menu() {
-  return <div>Manu</div>;
-}
+const Menu = ({ cartCount }) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            Cart(<span>{cartCount}</span>)
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Menu;
