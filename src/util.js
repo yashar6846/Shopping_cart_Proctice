39d5f389021b702a43ace41aa2f71a6014dd28a2
yashar6846 <yasharnajafi6846@gmail.com>
2021-05-31 10:26:19 +0300

    testremove from cart

--- src/util.js
+++ src/util.js
@@ -1,25 +1,38 @@
 export const Add = (num1 = 0, num2 = 0) => {
-    return num1 + num2
-}
-
-// test('Add undefined with a number', () => {
-//     const result = Add(undefined, 3)
-//     expect(result).toBe(3)
-// })
-
-export const AddItemToList = (list, newItem) => {
-    let isNew = true
+    return num1 + num2;
+  };
+  
+  // test('Add undefined with a number', () => {
+  //     const result = Add(undefined, 3)
+  //     expect(result).toBe(3)
+  // })
+  
+  export const AddItemToList = (list, newItem) => {
+    let isNew = true;
     const newList = list
-        ? list.map((item) => {
-              if (item.id === newItem.id) {
-                  item.count++
-                  isNew = false
-              }
-              return item
-          })
-        : []
+      ? list.map((item) => {
+          if (item.id === newItem.id) {
+            item.count++;
+            isNew = false;
+          }
+          return item;
+        })
+      : [];
     if (isNew) {
-        newList.push({ ...newItem, count: 1 })
+      newList.push({ ...newItem, count: 1 });
     }
-    return newList
-}
+    return newList;
+  };
+  
+  export const RemoveOneFromList = (List, item) => {
+    const newList = List.map((ListItem) => {
+      const newItem = { ...ListItem };
+      if (ListItem.id === item.id) {
+        newItem.count--;
+      }
+      return newItem;
+    });
+  
+    return newList.filter((ListItem) => ListItem.count > 0);
+  };
+  
--- src/util.test.js
+++ src/util.test.js
@@ -1,42 +1,63 @@
-import { Add, AddItemToList } from './util'
+import { Add, AddItemToList, RemoveOneFromList } from "./util";
 
-test('Add 2 numbers', () => {
-    const result = Add(2, 3)
-    expect(result).toBe(5)
-})
+test("Add 2 numbers", () => {
+  const result = Add(2, 3);
+  expect(result).toBe(5);
+});
 
-describe('AddItemToList function', () => {
-    const item1 = {
-        id: '1OUgYsXNA8ZSTBcV9BEmZD',
-    }
-    const item2 = {
-        id: '2',
-    }
-    test('add an item to empty list', () => {
-        const newList1 = AddItemToList(undefined, item1)
-        expect(newList1.length).toBe(1)
-        expect(newList1[0].count).toBe(1)
+describe("AddItemToList function", () => {
+  const item1 = {
+    id: "1OUgYsXNA8ZSTBcV9BEmZD",
+  };
+  const item2 = {
+    id: "2",
+  };
+  test("add an item to empty list", () => {
+    const newList1 = AddItemToList(undefined, item1);
+    expect(newList1.length).toBe(1);
+    expect(newList1[0].count).toBe(1);
 
-        const newList2 = AddItemToList(newList1, item2)
-        expect(newList2.length).toBe(2)
-        expect(newList2[1].count).toBe(1)
+    const newList2 = AddItemToList(newList1, item2);
+    expect(newList2.length).toBe(2);
+    expect(newList2[1].count).toBe(1);
 
-        const newList3 = AddItemToList(newList2, item1)
-        expect(newList3.length).toBe(2)
-        expect(newList3[0].count).toBe(2)
-    })
+    const newList3 = AddItemToList(newList2, item1);
+    expect(newList3.length).toBe(2);
+    expect(newList3[0].count).toBe(2);
+  });
 
-    test('add an item to the list with one different item in it', () => {
-        const newList1 = AddItemToList(undefined, item1)
-        const newList2 = AddItemToList(newList1, item2)
-        expect(newList2.length).toBe(2)
-        expect(newList2[1].count).toBe(1)
-    })
-    test('add an existing item to the list', () => {
-        const newList1 = AddItemToList(undefined, item1)
-        const newList2 = AddItemToList(newList1, item2)
-        const newList3 = AddItemToList(newList2, item1)
-        expect(newList3.length).toBe(2)
-        expect(newList3[0].count).toBe(2)
-    })
-})
+  test("add an item to the list with one different item in it", () => {
+    const newList1 = AddItemToList(undefined, item1);
+    const newList2 = AddItemToList(newList1, item2);
+    expect(newList2.length).toBe(2);
+    expect(newList2[1].count).toBe(1);
+  });
+  test("add an existing item to the list", () => {
+    const newList1 = AddItemToList(undefined, item1);
+    const newList2 = AddItemToList(newList1, item2);
+    const newList3 = AddItemToList(newList2, item1);
+    expect(newList3.length).toBe(2);
+    expect(newList3[0].count).toBe(2);
+  });
+});
+//RemoveOneFromList
+
+const list = [
+  { id: "item1", count: 7 },
+  { id: "item2", count: 2 },
+  { id: "item3", count: 1 },
+];
+
+test("remove list from list ", () => {
+  const newList = RemoveOneFromList(list, { id: "item1" });
+  expect(newList.length).toBe(3);
+  expect(newList[0].count).toBe(6);
+  expect(newList[1].count).toBe(2);
+  expect(newList[2].count).toBe(1);
+});
+test("remove item with 1 count from list ", () => {
+  const newList = RemoveOneFromList(list, { id: "item3" });
+  expect(newList.length).toBe(2);
+  expect(newList[0].count).toBe(7);
+  expect(newList[1].count).toBe(2);
+});
